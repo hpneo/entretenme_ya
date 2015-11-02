0111c427105b4df17673c91d03a632b79a6e9a2b
HPNeo <hpneo@hotmail.com>
2015-11-02 13:10:28 -0500

    Merge comment view in content view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102071450) do
+ActiveRecord::Schema.define(version: 20151102180651) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "content_id", limit: 4
@@ -25,15 +25,16 @@ ActiveRecord::Schema.define(version: 20151102071450) do
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "contents", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.integer  "year",        limit: 4
-    t.string   "description", limit: 255
-    t.string   "poster",      limit: 255
-    t.string   "trailer",     limit: 255
-    t.float    "score",       limit: 24
-    t.string   "country",     limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "name",         limit: 255
+    t.integer  "year",         limit: 4
+    t.string   "description",  limit: 255
+    t.string   "poster",       limit: 255
+    t.string   "trailer",      limit: 255
+    t.float    "score",        limit: 24
+    t.string   "country",      limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "content_type", limit: 255
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -45,6 +46,16 @@ ActiveRecord::Schema.define(version: 20151102071450) do
 
   add_index "favorites", ["content_id"], name: "index_favorites_on_content_id", using: :btree
   add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
+
+  create_table "pendings", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "content_id", limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "pendings", ["content_id"], name: "index_pendings_on_content_id", using: :btree
+  add_index "pendings", ["user_id"], name: "index_pendings_on_user_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "content_id", limit: 4
@@ -80,6 +91,8 @@ ActiveRecord::Schema.define(version: 20151102071450) do
   add_foreign_key "comments", "users"
   add_foreign_key "favorites", "contents"
   add_foreign_key "favorites", "users"
+  add_foreign_key "pendings", "contents"
+  add_foreign_key "pendings", "users"
   add_foreign_key "reviews", "contents"
   add_foreign_key "reviews", "users"
 end
