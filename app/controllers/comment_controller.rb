cb270e049db9c6a8abd285a8eac2cb18464bc991
Henry Marotta <henry_marotta@hotmail.com>
2015-11-16 17:08:47 -0500

    Fixed redirection in Create Comment

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -11,7 +11,7 @@ class CommentController < ApplicationController
 			@body = params[:body]
 			@comment = Comment.new(content_id: @content_id, user_id: @user_id, body: @body)
 			@comment.save
-			redirect_to '/media/show'
+			redirect_to '/contents/' + @content_id
 		else
 			redirect_to '/home'
 		end
@@ -20,6 +20,7 @@ class CommentController < ApplicationController
 	def list
 		@content_id = params[:content]
 		@comments = Comment.where(content_id: @content_id)
+		#rompetelitas estuvo aqui
 	end
 
 end
